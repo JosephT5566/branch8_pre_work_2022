b3ef485e5e9e6f4d3e8434af5dd7f025d38396c4
Your JosephTseng <zxp930110@hotmail.com.tw>
2022-03-16 18:23:44 +0800

    add icons and set import

---
+++ src/constants/icons.ts
@@ -0,0 +1,19 @@
+import account from 'assets/icons/icon_account.png';
+import menu from 'assets/icons/icon_menu.svg';
+import phone from 'assets/icons/icon_phone.svg';
+import price from 'assets/icons/icon_price.svg';
+import time from 'assets/icons/icon_time.svg';
+import location from 'assets/icons/icon_location.svg';
+import logo_black from 'assets/icons/logo_black.png';
+import logo_white from 'assets/icons/logo_white.png';
+
+export const ICONS = {
+	account,
+	menu,
+	phone,
+	price,
+	time,
+	location,
+	logo_black,
+	logo_white,
+};
