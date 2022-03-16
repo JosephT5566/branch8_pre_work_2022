b6715b180c8cc2475291867ddb14d81856c022be
Joseph Tseng <zxp930110@hotmail.com.tw>
2022-03-16 11:15:33 +0800

    set index page

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "styles/global.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import Header from "view/layout/Header";
+import { ContentContainer } from "components/base/Container";
+
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "styles/theme";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<Header />
+			<ContentContainer>
+				<App />
+			</ContentContainer>
+		</ThemeProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
