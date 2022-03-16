47dc4f033bfcf3fc15fdb273430cef2b67f7aa47
Your JosephTseng <zxp930110@hotmail.com.tw>
2022-03-16 16:25:57 +0800

    modify header style

--- src/view/layout/Header.tsx
+++ src/view/layout/Header.tsx
@@ -1,27 +1,26 @@
-import { styled } from "@mui/material/styles";
-import Navigation from "./Navigation";
+import { styled } from '@mui/material/styles';
+import Navigation from './Navigation';
 
-const HeaderContainer = styled("header")(({ theme }) => ({
+const HeaderContainer = styled('header')(({ theme }) => ({
 	height: theme.layout.header.height,
 	backgroundColor: theme.palette.common.white,
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "space-between",
-	paddingInline: "2em",
-	paddingRight: "4em",
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'space-between',
+	padding: '2em max(1rem, calc((100vw - 1200px) / 2))',
 
-	"& .logo": {
-		display: "flex",
-		gap: "1em",
-		alignItems: "center",
-		height: "100%",
-		textDecoration: "none",
+	'& .logo': {
+		display: 'flex',
+		gap: '1em',
+		alignItems: 'center',
+		height: '100%',
+		textDecoration: 'none',
 		color: theme.palette.text.secondary,
-		"&:hover": {
+		'&:hover': {
 			color: theme.palette.primary.main,
 		},
-		"& img": {
-			height: "100%",
+		'& img': {
+			height: '100%',
 		},
 	},
 }));
@@ -29,8 +28,8 @@ const HeaderContainer = styled("header")(({ theme }) => ({
 export default function Header() {
 	return (
 		<HeaderContainer>
-			<a className={"logo"} href="/">
-				<h2>{"D&A Hostel"}</h2>
+			<a className={'logo'} href="/">
+				<h2>{'D&A Hostel'}</h2>
 			</a>
 			<Navigation />
 		</HeaderContainer>
