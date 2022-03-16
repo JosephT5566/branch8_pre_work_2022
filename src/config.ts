e2fc20a5079e2ac4399fd0e12bfea67c4d55d36b
Your JosephTseng <zxp930110@hotmail.com.tw>
2022-03-16 16:50:22 +0800

    set api url

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const API_ENDPOINT =
+	'https://fakerapi.it/api/v1/custom?_quantity=3&title=city&image=image&description=text&date=dateTime&phone=phone&tag1=pokemon&tag2=pokemon&tag3=pokemon&address=streetAddress';
