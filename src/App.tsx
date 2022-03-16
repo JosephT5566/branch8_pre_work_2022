7f4b42e069acdca2742e045b059ceba653096083
Your JosephTseng <zxp930110@hotmail.com.tw>
2022-03-16 17:26:17 +0800

    apply components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { styled } from '@mui/material/styles';
+
+import Stores from 'view/payment/Stores';
+import Events from 'view/payment/Events';
+import { PageContainer } from 'components/base/Container';
+
+const EventAndNewsContainer = styled('div')(({ theme }) => ({
+	display: 'flex',
+	padding: '1rem max(1rem, calc((100vw - 1200px) / 2))',
+
+	[theme.breakpoints.down('md')]: {
+		flexDirection: 'column',
+	},
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<PageContainer>
+			<Stores />
+			<EventAndNewsContainer>
+				<Events />
+			</EventAndNewsContainer>
+		</PageContainer>
+	);
 }
 
 export default App;
